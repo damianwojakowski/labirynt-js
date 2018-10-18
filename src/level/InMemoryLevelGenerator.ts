f67fce20d6252ffe94d35320e9f2599b49fd09d0
Damian Wojakowski <wojakowski.damian@gmail.com>
2018-10-19 00:45:31 +0200

    update memory generator

--- src/level/InMemoryLevelGenerator.ts
+++ src/level/InMemoryLevelGenerator.ts
@@ -1,11 +1,67 @@
 export class InMemoryLevelGenerator {
 
+    private base = [];
+    private row = [];
+
     public generateLevel(): Array<Array<string>> {
-        return this.getHardcodedLevel();
+        return this.createLevel();
     }
 
     private createLevel(): Array<Array<string>> {
-        return
+        this.prepareBase(11, 11);
+
+        return this.base;
+    }
+
+    private prepareBase(x: number, y: number): void {
+        this.base = [];
+        for (let i = 0; i < x; i++) {
+            for (let j = 0; j < y; j++) {
+                if (this.isFirstRow(i)) {
+                    this.row.push('WW');
+                    continue;
+                }
+
+                if (this.isLastRow(i, x - 1)) {
+                    this.row.push('WW');
+                    continue;
+                }
+
+                this.fillMiddleRows(i, j, x, y);
+            }
+            this.base.push(this.row);
+            this.row = [];
+        }
+
+        console.log(this.base);
+    }
+
+    private fillMiddleRows(i, j, x, y): void {
+        // first vertical line
+        if (i !== 0 && j === 0 && i !== x - 1) {
+            this.row.push('WW');
+            return;
+        }
+
+        // last vertical line
+        if (i !== 0 && i !== x - 1 && j === y - 1) {
+            this.row.push('WW');
+            return;
+        }
+
+        if (!(j % 2) && !(i % 2)) {
+            this.row.push('WW');
+        } else {
+            this.row.push('OO');
+        }
+    }
+
+    private isFirstRow(index: number): boolean {
+        return index === 0;
+    }
+
+    private isLastRow(index: number, levelSize: number): boolean {
+        return index === levelSize;
     }
 
     private getHardcodedLevel(): Array<Array<string>> {

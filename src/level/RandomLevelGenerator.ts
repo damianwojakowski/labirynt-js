2411cdbcd5a4bc70b2fee18754467ed979a2c557
Damian Wojakowski <wojakowski.damian@gmail.com>
2018-10-23 12:17:24 +0200

    update maze generation algo

--- src/level/RandomLevelGenerator.ts
+++ src/level/RandomLevelGenerator.ts
@@ -8,7 +8,7 @@ export class RandomLevelGenerator {
     private levelHeight = 19;
     private startingPoint = {x: 2, y: 2};
 
-    private walls: Array<{}> = [];
+    private walls: Array<Wall> = [];
 
     //    W P W P W
     //    P C P C P
@@ -32,6 +32,7 @@ export class RandomLevelGenerator {
         //         Make the wall a passage and mark the unvisited cell as part of the maze.
         //         Add the neighboring walls of the cell to the wall list.
         //     Remove the wall from the list.
+        this.drawMaze();
 
         console.log(this.base);
         return this.base;
@@ -129,25 +130,25 @@ export class RandomLevelGenerator {
             // check if wall in north and add to walls list
             element = this.base[cell.x - 1] && this.base[cell.x - 1][cell.y];
             if (this.isWallOrPassage(element)) {
-                this.walls.push(this.buildElement(cell.x - 1, cell.y));
+                this.walls.push(this.buildWall(element, cell.x - 1, cell.y));
             }
 
             // check if wall in south
             element = this.base[cell.x + 1] && this.base[cell.x + 1][cell.y];
             if (this.isWallOrPassage(element)) {
-                this.walls.push(this.buildElement(cell.x + 1, cell.y));
+                this.walls.push(this.buildWall(element, cell.x + 1, cell.y));
             }
 
             // check if wall in east
             element = this.base[cell.x][cell.y + 1];
             if (this.isWallOrPassage(element)) {
-                this.walls.push(this.buildElement(cell.x, cell.y + 1));
+                this.walls.push(this.buildWall(element, cell.x, cell.y + 1));
             }
 
             // check if wall in west
-            let element = this.base[cell.x][cell.y - 1];
+            element = this.base[cell.x][cell.y - 1];
             if (this.isWallOrPassage(element)) {
-                this.walls.push(this.buildElement(cell.x, cell.y - 1));
+                this.walls.push(this.buildWall(element, cell.x, cell.y - 1));
             }
         }
 
@@ -155,10 +156,51 @@ export class RandomLevelGenerator {
             return element === this.levelElements.getVerticalPassage() || element === this.levelElements.getHorizontalPassage();
         }
 
-        private buildElement(positionX: number, positionY: number) {
-            return {
-                positionX: positionX,
-                positionY: positionY,
+        private buildWall(element: string, positionX: number, positionY: number): Wall {
+            return new Wall(element, positionX, positionY);
+        }
+
+    private drawMaze(): void {
+        let infiniteLoopCounter = 100000;
+        let iterations = 0;
+
+        // While there are walls in the list:
+        while (this.walls.length > 0 && iterations < infiniteLoopCounter) {
+            iterations++;
+            // Pick a random wall from the list. If only one of the two cells that the wall divides is visited, then:
+            let randomWall = this.getRandomWall();
+            if (this.isOneCellVisitedOfTwoDividedByWall(randomWall)) {
+                // Make the wall a passage and mark the unvisited cell as part of the maze.
+                // Add the neighboring walls of the cell to the wall list.
+            } else {
+                // Remove the wall from the list.
             }
         }
+    }
+
+        private getRandomWall(): Wall {
+            let randomNumber = Math.floor(Math.random() * this.walls.length);
+            //console.log('randomNumber: ' + randomNumber);
+            return this.walls[randomNumber];
+        }
+
+        private isOneCellVisitedOfTwoDividedByWall(wall: Wall): boolean {
+            if (wall.element === this.levelElements.getHorizontalPassage()) {
+                return true;
+            } else if (wall.element === this.levelElements.getHorizontalPassage()) {
+                return false
+            }
+        }
+}
+
+class Wall {
+    public positionX: number;
+    public positionY: number;
+    public element: string;
+
+    constructor(element: string, positionX: number, positionY: number) {
+        this.element = element;
+        this.positionX = positionX;
+        this.positionY = positionY
+    }
 }
